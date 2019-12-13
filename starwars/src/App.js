93081b694e297cab3a1b768987efdae292f74d7c
Robert <Robert.Elias@live.com>
2019-12-13 08:51:31 -0800

    Axios successfully responded

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 
-import Axios from 'axios';
 import React from 'react';
+import axios from 'axios';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -11,21 +11,21 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
-const [data, setData] = useState([]);
-useEffect(()=> {
+//const [data, setData] = useState([]);
+
   axios.get('https://swapi.co/api/people/')
   
   .then(response => {
    
  
-    
+    //setData(response.data.results)
     console.log(response.data.results)
     //console.log(response.data)
   })
   .catch(error =>{
     console.log("There is an error in the Axios call: ", error)
   })
-},[]);
+
 
   return (
     <div className="App">
