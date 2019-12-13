adeb3ee5dcf0da55b19a408bab30b2238aec7649
Robert <Robert.Elias@live.com>
2019-12-13 08:38:28 -0800

    Created Person.js file

---
+++ starwars/src/components/Person.js
@@ -0,0 +1,22 @@
+import {
+    Button,
+    Card,
+    CardBody,
+    CardSubtitle,
+    CardTitle,
+} from "reactstrap";
+import React, { Component } from 'react'
+
+import axios from "axios"
+
+const PersonCard =(props) =>{
+    
+        return (
+            <div>
+                
+            </div>
+        )
+    
+}
+
+export default PersonCard
